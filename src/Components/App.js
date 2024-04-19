bc6c7753da3d0a616d7fadb3ae70f07c83a0fa58
AnjaliThawani <anjaliwebdeveloper24@gmail.com>
2024-04-19 20:18:11 +0530

    initial commit

---
+++ src/Components/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = <h1>Hello ANjali</h1>
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
