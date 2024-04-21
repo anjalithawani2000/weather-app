fc9c653ce16f0eace56840e71d50fa6e9b237d2d
AnjaliThawani <anjaliwebdeveloper24@gmail.com>
2024-04-21 11:55:33 +0530

    added env file

--- config/apiInfo.js
+++ config/apiInfo.js
@@ -1,1 +1,9 @@
-export const CURRENT_WEATHER = "https://api.openweathermap.org/data/2.5/weather?q=Rajasthan,in&APPID=04004575891aa41f0cabe0ea2fbfb149"
+import path from 'path';
+import dotenv from 'dotenv';
+import { DEFAULT_STATE, DEFAULT_COUNTRY } from '../src/utils/constants';
+// Load environment variables from .env file using absolute path
+const EnvInfo = dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+const { parsed } = EnvInfo;
+
+export const CURRENT_WEATHER = `https://api.openweathermap.org/data/2.5/weather?q=${DEFAULT_STATE},${DEFAULT_COUNTRY}&APPID=${parsed.APPID}`
